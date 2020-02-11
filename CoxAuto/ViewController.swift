f8f3695a3d6797689b8257630a48f598ff62f624
Mihai Chis <mh_chis@yahoo.com>
2020-02-11 10:24:25 +0200

    Initial Commit

---
+++ CoxAuto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoxAuto
+//
+//  Created by Mihai Chis on 11/02/2020.
+//  Copyright © 2020 Mihai Chis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
